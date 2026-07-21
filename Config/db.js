87c9608bb1a1b2bf66cb237bbb941359939f2277
William dos Santos Hennemann <williamdossantoshennemann@gmail.com>
2026-07-21 09:43:55 -0300

    Banco pronto

--- Config/db.js
+++ Config/db.js
@@ -1,13 +1,16 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT || 3306),
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  ssl: {
+    ca: Buffer.from(process.env.DB_SSL_CA_BASE64, "base64").toString("utf8")
+  }
 });
 
 module.exports = pool;
